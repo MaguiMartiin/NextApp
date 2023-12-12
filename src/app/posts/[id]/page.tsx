b6a8348435c5d5555d55ae42688b4e07dcef893f
Magui Martin <magalimartin02@gmail.com>
2023-12-12 19:13:28 -0300

    post id

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,20 @@
-export default function Post ({params}: any) {
+import Link from "next/link"
+
+const fetchPostId = (id:any) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(res=>res.json())
+}
+
+export default async function Post ({params}: any) {
     const { id }= params
+    const post = await fetchPostId(id)
     return(
-        <div>Esto es un post {id}</div>
+        <article>
+            <h1 className=" text-violet-400 mt-[1rem] mb-[1rem]">{post.title}</h1>
+            <p>{post.body}</p>
+            <Link href={`https://jsonplaceholder.typicode.com/posts/${id}/comments`} className=" bg-gray-500 p-[0.5rem] mt-[1rem] w-fit flex">
+                Ver comentarios
+            </Link>
+        </article>
     )
 }
